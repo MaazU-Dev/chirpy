e8efdabcc8e581f21d7fe86a3c8c65e2de71113d
Maaz Umar <maaz.umar@cowlar.com>
2026-02-04 01:26:20 +0500

    Enhance HTTP server in main.go to include health check endpoint and update file server path handling.

--- main.go
+++ main.go
@@ -8,7 +8,14 @@ func main() {
 	rootFileDir := "."
 	port := "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(rootFileDir)))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(rootFileDir))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		msg := "OK"
+		w.Write([]byte(msg))
+
+	})
 	s := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
