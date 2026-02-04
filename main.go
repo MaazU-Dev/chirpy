31d79b42b83395cd4c6995d32434004e8d72a312
Maaz Umar <maaz.umar@cowlar.com>
2026-02-04 13:13:55 +0500

    Refactor HTTP server in main.go to include metrics tracking and reset functionality, and add readiness check in readiness.go.

--- main.go
+++ main.go
@@ -1,24 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	rootFileDir := "."
 	port := "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(rootFileDir))))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(200)
-		msg := "OK"
-		w.Write([]byte(msg))
-
-	})
+	var config apiConfig
+	mux.Handle("/app/", http.StripPrefix("/app/", config.middlewareMetricsInc(http.FileServer(http.Dir(rootFileDir)))))
+	mux.HandleFunc("/metrics", config.handlerMetrics)
+	mux.HandleFunc("/reset", config.handlerReset)
+	mux.HandleFunc("/health", handlerReadiness)
 	s := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 	s.ListenAndServe()
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	},
+	)
 }
