13abd296e01ccedc8567e3a68bfadf3b52c24d90
Maaz Umar <maaz.umar@cowlar.com>
2026-02-06 21:41:33 +0500

    Add JWT authentication support with token generation and validation. Introduce unit tests for password hashing and JWT functionality. Update go.mod and go.sum to include golang-jwt dependency.

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,7 +1,18 @@
 package auth
 
 import (
+	"errors"
+	"time"
+
 	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type TokenType string
+
+const (
+	TokenAccessType TokenType = "chirpy-access"
 )
 
 func HashPassword(password string) (string, error) {
@@ -18,4 +29,45 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 		return false, err
 	}
 	return same, nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	now := time.Now()
+	expiryTime := now.Add(expiresIn)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    string(TokenAccessType),
+		IssuedAt:  jwt.NewNumericDate(now),
+		ExpiresAt: jwt.NewNumericDate(expiryTime),
+		Subject:   userID.String(),
+	})
+	signedToken, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+	id, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	issuer, err := token.Claims.GetIssuer()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if issuer != string(TokenAccessType) {
+		return uuid.Nil, errors.New("invalid issuer")
+	}
+	parsedUuid, err := uuid.Parse(id)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return parsedUuid, nil
 }
